962433764d60ea65380d887be3ab66087f0951ae
isalevine <isa.loves.coding@gmail.com>
2019-10-02 21:19:16 -0700

    successfully rotating 3x3 matrix

--- rotate.rb
+++ rotate.rb
@@ -1,7 +1,7 @@
 matrix = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]
 rotations = 1
 
-# [ [1, 2, 3]
+# [ [1, 2, 3]           
 #   [4, 5, 6]
 #   [7, 8, 9] ]
 
@@ -12,8 +12,44 @@ expected_output = [ [1, 4, 3], [8, 5, 2], [7, 6, 9] ]
 #   [7, 6, 9] ]
 
 
-def rotate(matrix, rotations)
-    # puts "Hi!"
+def rotate(matrix, rotations, expected_output)
+    last_index = matrix.length - 1
+    # recursively call this with a 'depth' variable to determine right_num index (replace -1)
+    matrix[0].each_with_index do |num, i|
+        if i != 0 && i != last_index
+            top_num = num   # matrix[0][i]
+            right_num = matrix[i][-1]           # matrix[i][depth - 1]  ??
+            bot_num = matrix[-1][-i - 1]
+            left_num = matrix[-i - 1][0]            # matrix[i][depth]      ??
+
+            matrix[0][i] = left_num     # top
+            matrix[i][-1] = top_num     # right
+            matrix[-1][-i - 1] = right_num  # bottom
+            matrix[-i - 1][0] = bot_num     # left
+        end
+    end
+
+    if matrix == expected_output
+        puts <<-SUCCESS
+            SUCCESS!!
+
+            matrix:
+            #{matrix}
+
+            expected_output:
+            #{expected_output}
+        SUCCESS
+    else
+        puts <<-FAIL
+            Failed...
+
+            matrix:
+            #{matrix}
+
+            expected_output:
+            #{expected_output}
+        FAIL
+    end
 end
 
-rotate(matrix, rotations)
+rotate(matrix, rotations, expected_output)
