8449ad106d154a79ae77972298f761241cd74694
isalevine <isa.loves.coding@gmail.com>
2019-10-02 21:01:56 -0700

    create empty rotate() function

--- rotate.rb
+++ rotate.rb
@@ -5,8 +5,15 @@ rotations = 1
 #   [4, 5, 6]
 #   [7, 8, 9] ]
 
-expected_output = [ [1, 4, 3], [8, 5, 2] [7, 6, 9] ]
+expected_output = [ [1, 4, 3], [8, 5, 2], [7, 6, 9] ]
 
 # [ [1, 4, 3]
 #   [8, 5, 2]
 #   [7, 6, 9] ]
+
+
+def rotate(matrix, rotations)
+    # puts "Hi!"
+end
+
+rotate(matrix, rotations)
