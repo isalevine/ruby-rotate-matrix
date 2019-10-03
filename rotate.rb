ee014721944de8eb1bcbd4337abc69aa932af0fa
isalevine <isa.loves.coding@gmail.com>
2019-10-02 22:15:25 -0700

    adding 4x4 matrix for test 2

--- rotate.rb
+++ rotate.rb
@@ -1,34 +1,61 @@
-matrix = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]
+# #   TEST 1
+# # ===========
+# matrix = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]
+# rotations = 1
+
+# # [ [1, 2, 3]           
+# #   [4, 5, 6]
+# #   [7, 8, 9] ]
+
+# expected_output = [ [1, 4, 3], [8, 5, 2], [7, 6, 9] ]
+
+# # [ [1, 4, 3]
+# #   [8, 5, 2]
+# #   [7, 6, 9] ]
+
+
+
+#   TEST 2
+# ===========
+matrix = [ [1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16] ]
 rotations = 1
 
-# [ [1, 2, 3]           
-#   [4, 5, 6]
-#   [7, 8, 9] ]
+# [ [1, 2, 3, 4]           
+#   [5, 6, 7, 8]
+#   [9,10,11,12] 
+#   [13,14,15,16] ]
 
-expected_output = [ [1, 4, 3], [8, 5, 2], [7, 6, 9] ]
+expected_output = [ [1, 9, 5, 4], [14, 6, 7, 2], [15, 10, 11, 3], [13, 12, 8, 16] ]
 
-# [ [1, 4, 3]
-#   [8, 5, 2]
-#   [7, 6, 9] ]
+# [ [ 1, 9, 5, 4]           
+#   [14, 6, 7, 2]
+#   [15,10,11, 3] 
+#   [13,12, 8,16] ]
+
+
+
 
 
 def rotate(matrix, rotations, expected_output)
     last_index = matrix.length - 1
     max_depth = (matrix.length / 2).floor
     depth = 0
-    # recursively call this with a 'depth' variable to determine right_num index (replace -1)
-    matrix[0].each_with_index do |num, i|
-        if i != 0 && i != last_index
-            top_num = num   # matrix[0][i]
-            right_num = matrix[i][-1]           # matrix[i][depth - 1]  ??
-            bot_num = matrix[-1][-i - 1]
-            left_num = matrix[-i - 1][0]            # matrix[i][depth]      ??
+    while depth < max_depth
+        # recursively call this with a 'depth' variable to determine right_num index (replace -1)
+        matrix[depth].each_with_index do |num, i|   # refactor to just use .each_index?
+            if i != 0 && i != last_index
+                top_num = num                 
+                right_num = matrix[i][depth - 1]    
+                bot_num = matrix[-1][-i - 1]
+                left_num = matrix[-i - 1][depth]
 
-            matrix[0][i] = left_num     # top
-            matrix[i][-1] = top_num     # right
-            matrix[-1][-i - 1] = right_num  # bottom
-            matrix[-i - 1][0] = bot_num     # left
+                matrix[depth][i] = left_num         # top
+                matrix[i][depth - 1] = top_num      # right
+                matrix[-1][-i - 1] = right_num      # bottom
+                matrix[-i - 1][depth] = bot_num     # left
+            end
         end
+        depth += 1
     end
 
     status = String.new
